b07c7af14364cebde201b284a46a666492046f64
ibul <shohibulkahfi80@gmail.com>
2026-02-04 09:52:39 +0700

    coba1

---
+++ PBO_LATIHAN_2407065/src/latihan1/main.java
@@ -0,0 +1,29 @@
+package latihan1;
+
+class Cafe {
+    String nama;
+    String alamat;
+
+    // Constructor
+    Cafe() {
+        this.nama = "default nama";
+        this.alamat = "default alamat";
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+
+        // Membuat objek
+        Cafe cafe1 = new Cafe();
+
+        // Mengubah data
+        cafe1.nama = "Kopi Kenangan";
+        cafe1.alamat = "Indramayu";
+
+        // Output
+        System.out.println("Praktikum pertama");
+        System.out.println("Nama Cafe   : " + cafe1.nama);
+        System.out.println("Alamat Cafe : " + cafe1.alamat);
+    }
+}
