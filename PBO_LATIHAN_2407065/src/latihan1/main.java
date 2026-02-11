a0a92122d91da4b6b8b7846929498b47b6636dea
ibul <shohibulkahfi80@gmail.com>
2026-02-11 09:24:10 +0700

    tamabah void buka namabah dikit

--- PBO_LATIHAN_2407065/src/latihan1/main.java
+++ PBO_LATIHAN_2407065/src/latihan1/main.java
@@ -1,36 +1,67 @@
 package latihan1;
 
+// import java.util.Scanner;
+
 class Cafe {
     String nama;
     String alamat;
 
     // Constructor
-    Cafe() {
-        this.nama = "default nama";
-        this.alamat = "default alamat";
-        System.out.println("nama cafe adalah " + this.nama);
-        System.out.println("alamat cafe adalah " + this.alamat);    
+    Cafe(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+        System.out.println(nama);
+        System.out.println(alamat);    
     }
 
+    // No-arg constructor
+   // Cafe() {
+     //   this.nama = "default nama";
+     //   this.alamat = "default alamat";
+     //   System.out.println("nama cafe adalah " + this.nama);
+    //    System.out.println("alamat cafe adalah " + this.alamat);
+  //  }
+
+    // Method
     void buka() {
         System.out.println("Cafe buka pada pukul 08.00");
     }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+
 }
 
-public class main {
+public class Main {
     public static void main(String[] args) {
 
-        // Membuat objek
-        Cafe cafe1 = new Cafe();
-        cafe1.buka();
+       // scanner input = new Scanner(System.in)) {
+       //     Cafe cafe = new Cafe();
 
-        // Mengubah data
-        cafe1.nama = "Kopi Kenangan";
-        cafe1.alamat = "Indramayu";
+         //   System.out.println("Masukkan nama cafe: ");
+         //   cafe.setNama(input.nextLine());
 
-        // Output
-        System.out.println("Praktikum pertama");
-        System.out.println("Nama Cafe   : " + cafe1.nama);
-        System.out.println("Alamat Cafe : " + cafe1.alamat);
+         //   System.out.println("nama yang di input adalah : " + cafe.nama);
+         //   cafe.buka();
+        
+
+        Cafe cafe2 = new Cafe("Kopi Kenangan", "Jl. Merdeka No. 10");
+        cafe2.setNama("Kopi Kenangan");
+        cafe2.buka();
+
     }
 }
